770b7edfdeaf0baa4056c8d956dbcbca696d742c
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-09 03:42:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = [0, 1, 2, 3]
-  array << 4
+  element = 4
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
